ba9b9c8877d0fe98d8c13fa148c37917116dcb72
marisarze <flypetrov@yandex.ru>
2022-07-14 16:29:24 +0300

    added simple test for run version

--- test_version.cpp
+++ test_version.cpp
@@ -1,3 +1,10 @@
 #include "lib.h"
-
 #include "gtest/gtest.h"
+#include "version.h"
+
+
+TEST(TestSimple, TestVersion){
+    auto result = version();
+    auto expected = PROJECT_VERSION_PATCH;
+    ASSERT_EQ(result, expected);
+};
