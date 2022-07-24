162077370ce85e904df5c9a2d04f53bfa5451c4d
marisarze <flypetrov@yandex.ru>
2022-07-24 16:12:20 +0300

    trying to remove build folder from commit history

--- build/CMakeFiles/helloworld.dir/lib.cpp.obj.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/helloworld.dir/lib.cpp.obj: \
- C:\Users\marisarze\Documents\otus_cpp\lib.cpp \
- C:\Users\marisarze\Documents\otus_cpp\lib.h \
- C:/Users/marisarze/Documents/otus_cpp/build/version.h
--- build/CMakeFiles/test_version.dir/test_version.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/test_version.dir/test_version.cpp.obj: \
- C:\Users\marisarze\Documents\otus_cpp\test_version.cpp \
- C:\Users\marisarze\Documents\otus_cpp\lib.h
